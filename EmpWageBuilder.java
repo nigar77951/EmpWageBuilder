f518733fe68ac1312de39f88d8fc1c61a28cf2b5
unknown <nigar77951@gmail.com>
2021-01-11 11:37:18 +0530

    emp wage cacl if present for full time

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,22 +1,29 @@
+
 public class EmpWageBuilder {
 	public static void main(String[] args) {
 
-	    System.out.println("...To check Employee Attendance...");
+        System.out.println("...to Calculate Employee wage for full time if present...");
 
-	    // Initialization
-	    final int IS_FULL_TIME = 1;
+        // Initialization
+        final int IS_FULL_TIME = 1;
+        final int EMP_RATE_PER_HR = 20;
 
-	    // random function
-	    double empCheck = Math.floor(Math.random() * 10) % 2;
-	    System.out.println(empCheck);
+        int empHrs = 0;
+        int empWage = 0;
 
-	    // wage computation
-	    if (empCheck == IS_FULL_TIME) {
-	        System.out.println("Employee is Present.");
-	    } else {
-	        System.out.println("Employee is Absent.");
-	    }
-		}
+        // random function
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        System.out.println(empCheck);
+
+        // wage computation
+        if (empCheck == IS_FULL_TIME) {
+            System.out.println("Employee is Present.");
+            empHrs = 8;
+        } else {
+            System.out.println("Employee is Absent.");
+            empHrs = 0;
+        }
+        empWage = empHrs * EMP_RATE_PER_HR;
+        System.out.println("Employee Wage : " + empWage);
+	}
 }
-
-
