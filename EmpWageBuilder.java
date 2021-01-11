d4680caeca2dd2e23341a02ea574b08c0cf091af
unknown <nigar77951@gmail.com>
2021-01-11 11:30:26 +0530

    calc wage if emp is present

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,6 +1,22 @@
 public class EmpWageBuilder {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        System.out.println("...Welcome to Employee Wage Builder...");
-    }
+	    System.out.println("...To check Employee Attendance...");
+
+	    // Initialization
+	    final int IS_FULL_TIME = 1;
+
+	    // random function
+	    double empCheck = Math.floor(Math.random() * 10) % 2;
+	    System.out.println(empCheck);
+
+	    // wage computation
+	    if (empCheck == IS_FULL_TIME) {
+	        System.out.println("Employee is Present.");
+	    } else {
+	        System.out.println("Employee is Absent.");
+	    }
+		}
 }
+
+
