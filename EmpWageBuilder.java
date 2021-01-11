f1791c3ba2a6e3030f29b2e7a2a1b5fa3fb482ca
unknown <nigar77951@gmail.com>
2021-01-11 11:46:39 +0530

    calc wage for full or part time

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,32 +1,35 @@
 public class EmpWageBuilder {
-	public static void main(String[] args) {
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HR = 20;
 
-	System.out.println("...to Calculate Employee wage for full time or part time if present...");
+    public static void main(String[] args) {
 
-	// Initialization
-    final int IS_PART_TIME = 1;
-    final int IS_FULL_TIME = 2;
-    final int EMP_RATE_PER_HR = 20;
+    	System.out.println("...to Calculate Employee wage for full time or part time if present...");
 
-    int empHrs = 0;
-    int empWage = 0;
+        // Initialization
+        int empHrs = 0;
+        int empWage = 0;
 
-    // random function
-    double empCheck = Math.floor(Math.random() * 10) % 3;
-    System.out.println(empCheck);
+        // random function
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-    // wage Computation
-    if (empCheck == IS_PART_TIME) {
-        System.out.println("Employee is Present.");
-        empHrs = 4;
-    } else if (empCheck == IS_FULL_TIME) {
-        System.out.println("Employee is Present.");
-        empHrs = 8;
-    } else {
-        System.out.println("Employee is Absent.");
-        empHrs = 0;
+        // wage computation
+        switch (empCheck) {
+            case IS_PART_TIME:
+                System.out.println("Employee is Present.");
+                empHrs = 4;
+                break;
+            case IS_FULL_TIME:
+                System.out.println("Employee is Present.");
+                empHrs = 8;
+                break;
+            default:
+                System.out.println("Employee is Absent.");
+                empHrs = 0;
+                break;
+        }
+        empWage = empHrs * EMP_RATE_PER_HR;
+        System.out.println("Employee Wage : " + empWage);
     }
-    empWage = empHrs * EMP_RATE_PER_HR;
-    System.out.println("Employee Wage : " + empWage);
-	}
 }
