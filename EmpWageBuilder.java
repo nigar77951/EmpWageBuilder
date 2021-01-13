e07a61c0fe8993831a377f36d93c70f91a58f5fe
unknown <nigar77951@gmail.com>
2021-01-13 09:49:50 +0530

    code refactor

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,23 +1,26 @@
 public class EmpWageBuilder {
-    public static final int IS_PART_TIME = 1;
+	public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HR = 20;
     public static final int NUM_OF_WORKING_DAYS = 20;
     public static final int MAX_HRS_IN_MONTH = 100;
 
-	public static int calcEmpWageForCompany(String company, int empRate, int numOfDays, int maxHrs) {
-		   // Initialization
+    //method to compute wage
+    public static int computeEmpWage() {
+
+		// Variables
         int empHrs = 0;
+        int empWage = 0;
         int totalEmpWage = 0;
         int totalWorkingDays = 0;
         int totalEmpHrs = 0;
 
-        // random function
-        while (totalEmpHrs <= maxHrs && totalWorkingDays < numOfDays) {
+        // Computation
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
             totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-            // wage computation
+		    // Case Checking
             switch (empCheck) {
                 case IS_PART_TIME:
                     empHrs = 4;
@@ -32,13 +35,14 @@ public class EmpWageBuilder {
             totalEmpHrs += empHrs;
             System.out.println("Day: " + totalWorkingDays + "\tEmp Hr: " + empHrs);
         }
-        totalEmpWage = totalEmpHrs * empRate;
-        System.out.println("Total Employee Wage for a company: " +company+" is: "+ totalEmpWage);
-		return totalEmpWage;
-	}
-	public static void main(String[] args) {
-		calcEmpWageForCompany("Microsoft", 20, 2, 10);
-		calcEmpWageForCompany("Amazon", 30, 3, 10);
+        return totalEmpWage = totalEmpHrs * EMP_RATE_PER_HR;
+    }
 
-	}
+	//main method
+    public static void main(String[] args) throws Exception {
+
+        // Welcome message
+        System.out.println("...Employee Wage Builder code refactor...");
+        System.out.println("Total Employee Wage: " + computeEmpWage());
+    }
 }
