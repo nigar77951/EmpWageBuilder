bf26d694ffe3b2bfaa03a57af87cf4500d0011e3
nigar77951 <75714294+nigar77951@users.noreply.github.com>
2021-01-14 10:42:02 +0530

    Update EmpWageBuilder.java

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,5 +1,5 @@
 public class EmpWageBuilder {
-	public static final int IS_PART_TIME = 1;
+    public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HR = 20;
     public static final int NUM_OF_WORKING_DAYS = 20;
@@ -8,7 +8,7 @@ public class EmpWageBuilder {
     //method to compute wage
     public static int computeEmpWage() {
 
-		// Variables
+	// Variables
         int empHrs = 0;
         int empWage = 0;
         int totalEmpWage = 0;
@@ -20,7 +20,7 @@ public class EmpWageBuilder {
             totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-		    // Case Checking
+	    // Case Checking
             switch (empCheck) {
                 case IS_PART_TIME:
                     empHrs = 4;
@@ -38,7 +38,7 @@ public class EmpWageBuilder {
         return totalEmpWage = totalEmpHrs * EMP_RATE_PER_HR;
     }
 
-	//main method
+    //main method
     public static void main(String[] args) throws Exception {
 
         // Welcome message
